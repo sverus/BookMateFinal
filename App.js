9fda513fb1ecc9f7219665a36538072a52c76d8e
Sammybrother@hotmail.com <Sammybrother@hotmail.com>
2020-09-22 11:40:41 +0930

    Added basic search box and button to implement fetch functionality for

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+ AppRegistry,
+ Button,
+ TextInput,
+ Alert
+} from 'react-native';
+
+
 
 export default function App() {
+
+    const [value, onChangeText] = React.useState('Enter a book or author');
   return (
     <View style={styles.container}>
-      <Text>Test</Text>
+      <Text>BookMate</Text>
+      <Text>Search for a book</Text>
+      <TextInput
+  style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+  onChangeText={text => onChangeText(text)}
+  value={value}
+/>
+      <Button
+        onPress={() => {
+          Alert.alert('You tapped the button!');
+        }}
+        title="Press Me"
+      />
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
